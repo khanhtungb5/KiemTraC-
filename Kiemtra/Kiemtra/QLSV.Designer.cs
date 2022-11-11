86831507759d78bc8a77f9a2f07f6b76a0597ef9
khanhtungb5 <114633270+khanhtungb5@users.noreply.github.com>
2022-11-11 19:58:15 +0700

    Add files via upload

---
+++ Kiemtra/Kiemtra/QLSV.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace Kiemtra
+{
+    partial class QLSV
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.btnBrowes = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.cboSheet = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Chọn File Name";
+            // 
+            // txtFilename
+            // 
+            this.txtFilename.Location = new System.Drawing.Point(154, 37);
+            this.txtFilename.Name = "txtFilename";
+            this.txtFilename.Size = new System.Drawing.Size(514, 23);
+            this.txtFilename.TabIndex = 1;
+            // 
+            // btnBrowes
+            // 
+            this.btnBrowes.Location = new System.Drawing.Point(683, 37);
+            this.btnBrowes.Name = "btnBrowes";
+            this.btnBrowes.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowes.TabIndex = 2;
+            this.btnBrowes.Text = "........";
+            this.btnBrowes.UseVisualStyleBackColor = true;
+            this.btnBrowes.Click += new System.EventHandler(this.btnBrowes_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(1, 146);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(800, 307);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // cboSheet
+            // 
+            this.cboSheet.FormattingEnabled = true;
+            this.cboSheet.Location = new System.Drawing.Point(154, 66);
+            this.cboSheet.Name = "cboSheet";
+            this.cboSheet.Size = new System.Drawing.Size(121, 23);
+            this.cboSheet.TabIndex = 4;
+            this.cboSheet.SelectedIndexChanged += new System.EventHandler(this.cboSheet_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Sheet";
+            // 
+            // QLSV
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cboSheet);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnBrowes);
+            this.Controls.Add(this.txtFilename);
+            this.Controls.Add(this.label1);
+            this.Name = "QLSV";
+            this.Text = " Read excel *.xls or * .xlxs";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtFilename;
+        private Button btnBrowes;
+        private DataGridView dataGridView1;
+        private ComboBox cboSheet;
+        private Label label2;
+    }
+}
---
+++ Kiemtra/Kiemtra/QLSV.cs
@@ -0,0 +1,55 @@
+﻿using ExcelDataReader;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kiemtra
+{
+    public partial class QLSV : Form
+    {
+        public QLSV()
+        {
+            InitializeComponent();
+        }
+
+        private void cboSheet_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataTable dt = tableCollection[cboSheet.SelectedItem.ToString()];
+            dataGridView1.DataSource = dt;
+        }
+        DataTableCollection tableCollection;
+        private void btnBrowes_Click(object sender, EventArgs e)
+        {
+            using(OpenFileDialog openFileDialog=new OpenFileDialog())
+            {
+                if(openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    txtFilename.Text = openFileDialog.FileName; 
+                    using (var stream = File.Open(openFileDialog.FileName, FileMode.Open,FileAccess.Read))
+                    {
+                        System.Text.Encoding.RegisterProvider(System.Text.CodePagesEncodingProvider.Instance);
+                        using (IExcelDataReader reader = ExcelReaderFactory.CreateCsvReader(stream))
+                        {
+                            DataSet result = reader.AsDataSet(new ExcelDataSetConfiguration()
+                            {
+                                ConfigureDataTable = (_) => new ExcelDataTableConfiguration() { UseHeaderRow = true }
+                            });
+                            tableCollection = result.Tables;
+                            cboSheet.Items.Clear();
+                            foreach (DataTable table in tableCollection)
+                                cboSheet.Items.Add(table.TableName);
+
+                        }
+                    }    
+                    
+                }    
+            }
+        }
+    }
+}
